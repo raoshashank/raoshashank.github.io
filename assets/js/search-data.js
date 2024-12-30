63a220f4baf94c5e7c47837837005e39a5d198b1
raoshashank <raoshashank73@gmail.com>
2024-12-30 09:25:40 +0000

    Deploying to gh-pages from @ raoshashank/raoshashank.github.io@8d8e53266138e21ac49c19a14a34e3aa74b08802 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
             },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
