7dff2992cdfa381e3443d4b8022deeaa9e1e2e85
raoshashank <raoshashank73@gmail.com>
2024-12-30 10:20:54 +0000

    Deploying to gh-pages from @ raoshashank/raoshashank.github.io@bc363ee75ebf41b6794c11eb44d9fbe3c37f1b29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -389,16 +389,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-socrates-is-released-on-arxiv-with-accompanying-code",
+          title: 'SocRATES is released on arxiv with Accompanying code!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
