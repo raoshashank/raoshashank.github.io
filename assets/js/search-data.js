0fffbe33dc3675f34f556d2977fd6687288682b2
raoshashank <raoshashank73@gmail.com>
2025-01-01 16:19:09 +0000

    Deploying to gh-pages from @ raoshashank/raoshashank.github.io@c1e214924bcb25145e93ba77096617627c5b6071 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,13 +421,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/smarpall", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
